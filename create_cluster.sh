3c7f34d31c9e8398175122f0c4a9eb9d92aa8441
Gururaj S <gururaj.sudhindra@gmail.com>
2020-04-23 19:26:56 +0530

    Updated create_cluster.sh

--- create_cluster.sh
+++ create_cluster.sh
@@ -1,3 +1,3 @@
-eksctl create cluster -r ap-south-1 -n capstone -t t2.micro
+eksctl create cluster -r ap-south-1 -n capstone -t t2.medium
 kubectl apply -f k8s/cg-deploy.yml
 kubectl apply -f k8s/cg-service.yml
